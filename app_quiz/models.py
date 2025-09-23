e25c11d3c0d9414000663237080ab546c237b35c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-09-23 11:34:01 +0200

    Build models (quiz, question)

--- app_quiz/models.py
+++ app_quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=80, blank=False, null=False)
+    description = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    video_url = models.URLField(blank=False, null=False)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    question_title = models.CharField(blank=False, null=False, max_length=200)
+    question_options = models.JSONField(default=list)
+    answer = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
