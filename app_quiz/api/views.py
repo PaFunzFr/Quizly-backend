6fd244b687434cf1b30dc07d4d9635dd52f2d40e
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-09-23 16:56:29 +0200

    Register Quiz for admin panel

--- app_quiz/api/views.py
+++ app_quiz/api/views.py
@@ -1,7 +1,27 @@
-from rest_framework import views
+from rest_framework import views, status
 from rest_framework.response import Response
 
 from .serializers import QuizCreateSerializer
+
+import json
+import yt_dlp
+
+
+ydl_opts = {
+    'format': 'm4a/bestaudio/best',
+    "quiet": True,
+    "noplaylist": True,
+    'outtmpl': '%(id)s.%(ext)s' # save file as "VIDEO_ID.m4a"
+    # 'postprocessors': [{  # Extract audio using ffmpeg
+    #     'key': 'FFmpegExtractAudio',
+    #     'preferredcodec': 'm4a',
+    # }]
+}
+
+def download(url):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(url)
+
 
 class QuizCreateView(views.APIView):
     serializer_class = QuizCreateSerializer
@@ -9,5 +29,11 @@ class QuizCreateView(views.APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+            url = serializer.validated_data['video_url']
+            download(url)
+
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
